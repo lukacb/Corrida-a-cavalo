026f5d7aa0c32082ebe6783d2ffa0e67cb2af624
jmg-estevez <joao.estevez@sou.inteli.edu.br>
2026-01-26 11:02:35 -0300

    programando jogador

--- main.py
+++ main.py
@@ -30,3 +30,32 @@ PERSONAGENS = {
     "cavalo":   {"w": 64, "h": 48, "color": MARROM, "jump": 15, "grav": 0.95},
 }
 
+# ---------------- JOGADOR ----------------
+class Player:
+    def __init__(self, tipo="corredor"):
+        p = PERSONAGENS[tipo]
+        self.w, self.h = p["w"], p["h"]
+        self.color = p["color"]
+        self.jump_force = p["jump"]
+        self.gravity = p["grav"]
+        self.rect = pygame.Rect(110, GROUND_Y - self.h, self.w, self.h)
+        self.vy = 0
+        self.on_ground = True
+
+    def jump(self):
+        if self.on_ground:
+            self.vy = -self.jump_force
+            self.on_ground = False
+
+    def update(self):
+        if not self.on_ground:
+            self.vy += self.gravity
+            self.rect.y += int(self.vy)
+            if self.rect.bottom >= GROUND_Y:
+                self.rect.bottom = GROUND_Y
+                self.vy = 0
+                self.on_ground = True
+
+    def draw(self):
+        pygame.draw.rect(tela, self.color, self.rect, border_radius=6)
+
