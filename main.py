f0cceeda26f524b3e302060cc4ced18c84127e51
jmg-estevez <joao.estevez@sou.inteli.edu.br>
2026-01-26 11:00:51 -0300

    coisas iniciai

---
+++ main.py
@@ -0,0 +1,26 @@
+# hipismo_runner.py
+# Hipismo Runner – nome do jogador + placar final
+
+import pygame
+import sys
+import random
+
+pygame.init()
+WIDTH, HEIGHT = 900, 400
+tela = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Hipismo Runner")
+
+CLOCK = pygame.time.Clock()
+FPS = 60
+
+# Cores
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+VERDE = (34, 139, 34)
+AMARELO = (255, 200, 0)
+MARROM = (120, 72, 18)
+AZUL = (20, 120, 220)
+CINZA = (200, 200, 200)
+
+GROUND_Y = HEIGHT - 70
+
